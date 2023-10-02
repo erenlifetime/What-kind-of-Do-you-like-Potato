b087be34c4f6c24a043ca4ab7ad61106b24b2a96
erenlifetime <medya_194@hotmail.com>
2023-10-02 21:19:39 +0300

    Initial Commit

---
+++ Deneme1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Deneme1
+//
+//  Created by Eren lifetime on 2.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
